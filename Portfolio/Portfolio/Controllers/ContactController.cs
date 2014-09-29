bbb7338b6f89a0ea37b2bdd4c1cbd3e5568515a6
SeedPaths-User <lessdismissive@email.com>
2014-09-28 23:44:28 -0600

    Portfolio
    My first functioning website.

---
+++ Portfolio/Portfolio/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new Models.Contact());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Models.Contact contacts)
+        {
+            //create connection to database
+            Models.PortfolioEntities db = new Models.PortfolioEntities();
+            //add our contact to the new contactform
+            db.Contacts.Add(contacts);
+            //save changes
+            db.SaveChanges();
+            //kick user to thankyou page
+            return RedirectToAction("Thankyou", "Contact");
+        }
+
+        public ActionResult Thankyou()
+        {
+            return View();
+        }
+    }
+}
---
+++ Portfolio/Portfolio/Models/ContactValidation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//set up dataannotations
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models
+{
+    //set up the meta type for annoations regarding this class
+    [MetadataType(typeof(ContactValidation))]
+    //create a public partial class for where we are linking to
+    public partial class Contact
+    {
+
+    }
+    public class ContactValidation
+    {
+        //all properties are reqired
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required] [EmailAddress]
+        public string EmailAddress { get; set; }
+        [Required] [Phone]
+        public string ContactNumber { get; set; }
+        [Required] [MaxLength(1000)]
+        public string Comment { get; set; }
+
+    }
+}
